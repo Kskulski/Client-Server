1c5889f12d69616b31e85a7a0d0b38b1e96a6829
Kskulski <firestar345@gmail.com>
2019-11-12 23:39:55 -0600

    more

--- assignment2/nc.py
+++ assignment2/nc.py
@@ -5,21 +5,6 @@ def cli():
     pass
 
 @cli.command()
-@click.argument('n1', type=int)
-@click.argument('n2', type=int)
-@click.option('--i', is_flag=True)
-def divide(n1, n2, i):
-    try:
-        if i:
-            click.echo('result = {}'.format(n1 // n2))
-        else:
-            click.echo('result = {}'.format(n1 / n2))
-
-    except ZeroDivisionError:
-        click.echo('cannot divide by zero')
-
-
-@cli.command()
 @click.argument('host', default='127.0.0.1')
 @click.argument('port', type=int, default=80)
 def connect(port, host):
